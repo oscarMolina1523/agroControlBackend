7550db5c274a549f00a9ec7c18b0052678418bb2
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-07-22 17:53:32 -0600

    feat: configure to deploy, separate in dev and production

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,6 @@ import authRoutes from "./WebApi/routes/auth.routes";
 import { validateToken } from "./WebApi/middlewares/auth.middleware";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -44,6 +43,11 @@ app.get("/api-docs", async (req, res, next) => {
       next(error);
     }
   });
+
+app.get("/", (req, res) => {
+  res.send("Agro control System API working!");
+});
+
 //AUTO-REGISTER-ROUTES
 app.use("/auth", authRoutes);
 app.use("/auditLog", validateToken, auditLogRoutes);
